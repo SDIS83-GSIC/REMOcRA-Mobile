30b96826c578f090ba9615af63f00ac0127cd512
Emilie Genton <emg@atolcd.com>
2023-10-02 14:28:23 +0200

    Ajoute une lien vers le GPS dans l'infoWindow
    Issue: #190831
    Change-Id: Id7b40d54e1099a8b8cbe737ab410278a2ef30c31

--- app/src/main/java/fr/sdis83/remocra/mobile/ui/components/HydrantInfoWindow.kt
+++ app/src/main/java/fr/sdis83/remocra/mobile/ui/components/HydrantInfoWindow.kt
@@ -2,6 +2,7 @@ package fr.sdis83.remocra.mobile.ui.components
 
 import android.graphics.Color
 import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.view.MotionEvent
 import android.view.View
 import android.widget.TextView
@@ -15,6 +16,7 @@ import org.osmdroid.views.overlay.infowindow.InfoWindow
 class HydrantInfoWindow(mapView: MapView) : InfoWindow(R.layout.map_bubble, mapView) {
 
     val mHydrantNumero = R.id.hydrant_numero
+    val mOpenGps = R.id.open_gps
     var mHydrantDisponibilite = R.id.hydrant_disponibilite
     var mHydrantAdresse = R.id.hydrant_adresse
     var mHydrantAdresseComplement = R.id.hydrant_adresse_complement
@@ -33,18 +35,32 @@ class HydrantInfoWindow(mapView: MapView) : InfoWindow(R.layout.map_bubble, mapV
         try {
             val overlay = item as OverlayWithIW
             val hydrant = overlay.relatedObject as MapViewModel.HydrantGeoPoint
-            (mView.findViewById<View>(mHydrantNumero) as TextView).text = "Point d'eau N°${hydrant.numero}"
-            (mView.findViewById<View>(mHydrantDisponibilite) as TextView).apply {
+            getTextView(mHydrantNumero).text = "Point d'eau N°${hydrant.numero}"
+
+            getTextView(mHydrantDisponibilite).apply {
                 text = hydrant.dispoTerrestre?.name
                 setTextColor(if (hydrant.dispoTerrestre == Hydrant.Disponibilite.DISPO) Color.rgb(63, 191, 63) else Color.rgb(191, 63, 63))
             }
-            (mView.findViewById<View>(mHydrantAdresse) as TextView).text = hydrant.voie
-            (mView.findViewById<View>(mHydrantAdresseComplement) as TextView).text =
+
+            val linkGpsTextView = getTextView(mOpenGps)
+            linkGpsTextView.movementMethod = LinkMovementMethod.getInstance()
+            linkGpsTextView.text = Html.fromHtml(
+                "<a href=\"https://maps.google.com/?q=" +
+                    "${hydrant.lat},${hydrant.lon}\">Ouvrir le GPS</a>",
+                Html.FROM_HTML_MODE_COMPACT,
+            )
+
+            getTextView(mHydrantAdresse).text = hydrant.voie
+            getTextView(mHydrantAdresseComplement).text =
                 hydrant.observation
-            (mView.findViewById<View>(mHydrantCaracteristiques) as TextView).text = Html.fromHtml(hydrant.peiCaracteristiques, Html.FROM_HTML_MODE_COMPACT)
+
+            getTextView(mHydrantCaracteristiques).text = Html.fromHtml(hydrant.peiCaracteristiques, Html.FROM_HTML_MODE_COMPACT)
         } catch (e: Exception) {
         }
     }
+
+    private fun getTextView(rId: Int): TextView =
+        mView.findViewById<View>(rId) as TextView
 
     override fun onClose() {
         // no-op
